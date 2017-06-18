c24848a1195696124f11e2256e3e192a41f0d7f4
raoul1996 <baobaozhou199606@gmail.com>
2017-06-18 13:03:36 +0000

    修改项目

--- server/db/db.js
+++ server/db/db.js
@@ -2,28 +2,29 @@ const mongoose = require('mongoose')
 const config = require('config-lite')
 
 // mongodb 连接🔗
-mongoose.connect(config.mongodb)
+let links = 'mongodb://123.207.252.230:12345/test'
+mongoose.connect(links)
 // 此处防止 node.js - Mongoose: mpromise 错误
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
+mongoose.Promise = global.Promise
+let db = mongoose.connection
 db.on('error', console.error.bind(console, 'Connect error'))
 db.once('open', function () {
-	console.log('Mongodb started successfully')
+  console.log('Mongodb started successfully')
 })
 
 
-var userSchema = mongoose.Schema({
-	email: String,
-	password: String,
-	recheck: String,
-	token: String,
-	create_time: Date
+let userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+  recheck: String,
+  token: String,
+  create_time: Date
 })
 
-var model = {
-	// 在此处扩展 model，例如：
-	// Article: mongoose.model('Article', articleSchema),
-	User: mongoose.model('User', userSchema)
+let model = {
+  // 在此处扩展 model，例如：
+  // Article: mongoose.model('Article', articleSchema),
+  User: mongoose.model('User', userSchema)
 }
 
 module.exports = model
